60c434e4599be8074899adafe5dfc515a3a0fdfd
Maykal <m.m.droshev-2019@hull.ac.uk>
2020-12-02 10:29:53 +0000

    Changed the code so the program can run from Login Window

--- SQLiteDemo/Program.cs
+++ SQLiteDemo/Program.cs
@@ -4,10 +4,12 @@ using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace SQLiteDemo
 {
-   
+    
+
     class Program
     {
         static void Main(string[] args)
