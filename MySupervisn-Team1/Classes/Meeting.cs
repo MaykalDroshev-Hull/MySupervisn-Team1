a942d46cee31117351d25af3e1f015cff5932925
studiomfb <57592762+studioMFB@users.noreply.github.com>
2020-12-03 10:07:21 +0000

    new class: - DatabseManager: - Deals with: - Creat connection to database - Creating table - Saving data to table: - reading data from table
    in Meeting:
    - created new method:
    - SaveMeeting :
    - initiate new DatabaseManager to save meeting

--- MySupervisn-Team1/Classes/Meeting.cs
+++ MySupervisn-Team1/Classes/Meeting.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace MySupervisn_Team1
 {
     class Meeting
@@ -20,5 +21,14 @@ namespace MySupervisn_Team1
             throw new NotImplementedException();
         }
 
+        // In progress!!
+        // Perhaps better to have SaveMeetingDates for PS
+        // Then BookMeeting
+        // DeleteMeeting
+        public void SaveMeeting(string pTableName, int pId, string pName, DateTime pDate, List<string> pMessage)
+        {
+            DatabaseManager saveMeetingToDatabase = new DatabaseManager(pTableName, pId, pName, pDate, pMessage);
+        }
+      
     }
 }
