4bdb53284a0b215db667ab5f682528d60eefca96
Maykal <m.m.droshev-2019@hull.ac.uk>
2020-12-03 17:19:54 +0000

    Added functionality for login window

--- MySupervisn-Team1/LoginWindow.xaml.cs
+++ MySupervisn-Team1/LoginWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using MessageBox = System.Windows.MessageBox;
 
 namespace MySupervisn_Team1
 {
@@ -24,11 +27,59 @@ namespace MySupervisn_Team1
         {
             InitializeComponent();
         }
-
-
+        bool username_match = false;
+        bool password_match = false;
         private void Login_Click(object sender, RoutedEventArgs e)
         {
+            string username = Username.Text;
+            string password = Password.Password;
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\micha\Documents\GitHub\MySupervisn-Team1\MySupervisn-Team1\Users.mdf;Integrated Security=true";
+            conn.Open();
+            SqlCommand search = new SqlCommand();
+            search.CommandText = "select User_Id,password,Classification from [Table]";
+            search.Connection = conn;
+            SqlDataReader rd = search.ExecuteReader();
+            string Classification = "";
+            while (rd.Read())
+            {
+                if (rd[0].ToString() == username)
+                {
+                    username_match = true;
+                    if (rd[1].ToString() == password)
+                    {
+                        Classification = rd[2].ToString();
+                        password_match = true;
+                        break;
+                    }
+                    
+                }
+            }
             
+            if (username_match && password_match) {
+                switch (Classification)
+                {
+                    case "Student":
+                        break;
+                    case "Student Hub":
+                        
+                        break;
+                    case "Personal Supervisor":
+                        break;
+                    case "Director of Study":
+                        break;
+
+
+                }
+            }
+            else {
+              
+                   MessageBox.Show("Invalid credentials", "Access Denied", MessageBoxButton.OK, MessageBoxImage.Warning);
+                InitializeComponent();
+            
+            }
+
+
         }
     }
 }
