1d3bc06833ceb639b441d6db355a8f70ebc6c7b4
JLewis947 <29450687+JLewis947@users.noreply.github.com>
2020-12-10 10:06:36 +0000

    Updated connection string so that a global connection string can be used without having to change it for every machine. Changed an error on login page.

--- MySupervisn-Team1/LoginWindow.xaml.cs
+++ MySupervisn-Team1/LoginWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data.SqlClient;
 using MessageBox = System.Windows.MessageBox;
+using System.IO;
 
 namespace MySupervisn_Team1
 {
@@ -34,7 +35,8 @@ namespace MySupervisn_Team1
             string username = Username.Text;
             string password = Password.Password;
             SqlConnection conn = new SqlConnection();
-            conn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\micha\Documents\GitHub\MySupervisn-Team1\MySupervisn-Team1\DataBase\Users.mdf;Integrated Security=True";
+            var path = Environment.CurrentDirectory + @"\DataBase\Users.mdf";
+            conn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + path + ";Integrated Security=True";
             conn.Open();
             SqlCommand search = new SqlCommand();
             search.CommandText = "select User_Id,password,Classification,FirstName, LastName,email,password,Supervisor from [Table]";
@@ -66,7 +68,7 @@ namespace MySupervisn_Team1
                         break;
                     case "Student Hub":
                         this.Hide();
-                        Staff stf = new Staff(int.Parse(username), rd[3].ToString()+rd[4].ToString());
+                        Staff stf = new Staff(int.Parse(username), reader[3].ToString() + reader[4].ToString());
                         StaffDashboard staffDashboard = new StaffDashboard(stf);
                         staffDashboard.Show();
                         break;
