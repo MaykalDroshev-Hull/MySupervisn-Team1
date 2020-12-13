0527951742bf388869335c7afb7eab515904538d
Maykal <m.m.droshev-2019@hull.ac.uk>
2020-12-13 10:39:36 +0000

    Student Search is fully working

--- MySupervisn-Team1/BookMeeting.xaml.cs
+++ MySupervisn-Team1/BookMeeting.xaml.cs
@@ -36,6 +36,12 @@ namespace MySupervisn_Team1
             mStaff = pStaff;
         }
 
+        public BookMeeting(Staff pStaff, string name) : this(pStaff)
+        {
+            InitializeComponent();
+            people.Text = ($"{pStaff.Name};{name}");
+        }
+
         private void GoBack_Click(object sender, RoutedEventArgs e)
         {
             Close();
